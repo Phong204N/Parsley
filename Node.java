2d80fbe7b85dc14a55632e9ec57025ca9f2f290c
Phong Nguyen <phong204n@gmail.com>
2022-11-18 22:15:49 -0800

    Test Commit 1

---
+++ Node.java
@@ -0,0 +1,12 @@
+public class Node {
+    private char value;
+    private boolean isVar;
+    private Node [] productions; 
+
+    Node(char value, boolean isVar){
+        this.value = value;
+        this.isVar = isVar;
+    }
+
+    
+}
